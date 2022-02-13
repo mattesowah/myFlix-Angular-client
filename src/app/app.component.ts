cbd3f6cf9b28b0ebdb0666ab89e666cd4f411bba
a17labs <*a17matte@gmail.com*>
2022-02-13 15:23:53 +0000

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -27,10 +26,4 @@ openUserRegistrationDialog(): void {
     width: '480px'
     });
   }
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
-
 }
