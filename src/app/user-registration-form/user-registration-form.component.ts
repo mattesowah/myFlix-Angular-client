d595c0a0cfc48ed4b1d79ec316636bd04a155206
a17labs <*a17matte@gmail.com*>
2022-03-12 14:41:44 +0000

    changes

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,3 +1,7 @@
+/**
+ * The UserRegistrationFormComponent is used to register a new user.
+ * @module UserRegistrationFormComponent
+ */
 import { Component, OnInit, Input } from '@angular/core';
 
 // used to close the dialog on success
@@ -18,7 +22,9 @@ import { Router } from '@angular/router';
 })
 
 export class UserRegistrationFormComponent implements OnInit {
-
+    /**
+   * get input data of user and stores in userCredentials
+   */
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
   constructor(
@@ -28,9 +34,13 @@ export class UserRegistrationFormComponent implements OnInit {
     public router: Router
   ) { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  /**
+   * call API endpoint to register a new user
+   * @function userRegistration
+   * @param userCredentials {object}
+   * @return new users data in json format
+   */
   // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
