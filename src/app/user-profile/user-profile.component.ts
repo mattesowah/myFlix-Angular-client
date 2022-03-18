5c691e337f650726cf8fdb90106dacea559ad3ea
a17labs <*a17matte@gmail.com*>
2022-03-18 10:07:08 +0000

    addition of empty movie state

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -9,141 +9,77 @@ import { DirectorCardComponent } from '../director-card/director-card.component'
 import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss'],
+selector: 'app-user-profile',
+templateUrl: './user-profile.component.html',
+styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  user: any = {};
-  Username = localStorage.getItem('user');
-  Movie = localStorage.getItem('movie');
+user: any = {};
+movies: any = [];
+Username = localStorage.getItem('user');
+Movie = localStorage.getItem('movie');
 
-  FavMovies: any[] = [];
+FavMovies: any = [];
 
-  constructor(
-    public fetchApiData: FetchApiDataService,
-    public snackBar: MatSnackBar,
-    public dialog: MatDialog,
-    public router: Router
-  ) {}
+constructor(
+public fetchApiData: FetchApiDataService,
+public snackBar: MatSnackBar,
+public dialog: MatDialog,
+public router: Router
+) {}
 
-  ngOnInit(): void {
-    this.getUserInfo();
-    this.getFavoriteMovies();
-  }
-
-  /**
-   * call API end-point to get the user's information
-   * @function getUser
-   * @param Username
-   * @return user's data in json format
-   */
-  getUserInfo(): void {
-    const user = localStorage.getItem('user');
-    if (user) {
-      this.fetchApiData.getUser(user).subscribe((resp: any) => {
-        this.user = resp;
-
-        console.log(this.user);
-      });
-    }
-  }
-
-  /**
-   * get user's FavoriteMovies from the user's data
-   */
-  getFavoriteMovies(): void {
-    this.fetchApiData.getMovie(this.Movie!).subscribe((resp: any) => { this.FavMovies = resp.FavouriteMovies });
-  }
-
-
-
-  /**
-   * use API end-point to remove user favorite
-   * @function deleteFavoriteMovie
-   * @param MovieId {string}
-   * @param title {string}
-   * @returns updated user's data in json format
-   */
-  removeFavoriteMovie(MovieId: string, title: string): void {
-    this.fetchApiData.deleteFavoriteMovie(MovieId).subscribe((resp: any) => {
-      console.log(resp);
-      this.snackBar.open(
-        `${title} has been removed from your favorites!`,
-        'OK',
-        {
-          duration: 4000,
-        }
-      );
-      this.ngOnInit();
-    });
-  }
-
-  /**
-   * call API end-point to remove the current user
-   * @function deleteUser
-   * @param Username {any}
-   * @return remove status
-   */
-  deleteUser(): void {
-    this.fetchApiData.deleteUser(this.Username).subscribe(() => {
-      this.snackBar.open(`${this.Username} has been removed!`, 'OK', {
-        duration: 4000,
-      });
-      localStorage.clear();
-    });
-    this.router.navigate(['welcome']);
-  }
-
-  /**
-   * open a dialog to edit the user profile
-   * @module EditProfileFormComponent
-   */
-  openEditProfileFormDialog(): void {
-    this.dialog.open(EditProfileFormComponent, {
-      width: '280px',
-    });
-  }
-  /**
-   *open a dialog to display the GenreCardComponent
-   * @param name {string}
-   * @param description {string}
-   */
-  openGenreDialog(name: string, description: string): void {
-    this.dialog.open(GenreCardComponent, {
-      data: { name: name, description: description },
-      width: '300px',
-    });
-  }
-
-  /**
-   * open a dialog to display the DirectorCardComponent
-   * @param name {string}
-   * @param bio {string}
-   * @param birth {string}
-   * @param death {string}
-   */
-  openDirectorDialog(
-    name: string,
-    bio: string,
-    birth: string,
-    death: string
-  ): void {
-    this.dialog.open(DirectorCardComponent, {
-      data: { name: name, bio: bio, birth: birth, death: death },
-      width: '300px',
-    });
-  }
-
-  /**
-   * open a dialog to display the SynopsisCardComponent
-   * @param title {string}
-   * @param description {string}
-   */
-  openSynopsisDialog(title: string, description: string): void {
-    this.dialog.open(SynopsisCardComponent, {
-      data: { title: title, description: description },
-      width: '300px',
-    });
-  }
+ngOnInit(): void {
+this.getUserInfo();
 }
+
+/**
+
+call API end-point to get the user's information
+@function getUser
+@param Username
+@return user's data in json format */
+/**
+
+get user's FavoriteMovies from the user's data */ getFavoriteMovies(): void { this.movies.forEach((movie: any) => { if (this.user.FavoriteMovies.includes(movie._id)) { this.FavMovies.push(movie); } }); return this.FavMovies; }
+getUserInfo(): void {
+const user = localStorage.getItem('user');
+this.fetchApiData.getUser(user).subscribe((res: any) => {
+this.user = res;
+this.getMovies();
+});
+}
+
+/**
+
+retrieve all favorited movies */ getMovies(): void { this.fetchApiData.getAllMovies().subscribe((res: any) => { this.movies = res; this.getFavoriteMovies(); }); }
+/**
+
+use API end-point to remove user favorite
+@function deleteFavoriteMovie
+@param MovieId {string}
+@param title {string}
+@returns updated user's data in json format */ removeFavoriteMovie(MovieId: string, title: string): void { this.fetchApiData.deleteFavoriteMovie(MovieId).subscribe((resp: any) => { console.log(resp); this.snackBar.open( `${title} has been removed from your favorites!`, 'OK', { duration: 4000, } ); this.ngOnInit(); }); }
+/**
+
+call API end-point to remove the current user
+@function deleteUser
+@param Username {any}
+@return remove status */ deleteUser(): void { this.fetchApiData.deleteUser(this.Username).subscribe(() => { this.snackBar.open(`${this.Username} has been removed!`, 'OK', { duration: 4000, }); localStorage.clear(); }); this.router.navigate(['welcome']); }
+/**
+
+open a dialog to edit the user profile
+@module EditProfileFormComponent */ openEditProfileFormDialog(): void { this.dialog.open(EditProfileFormComponent, { width: '280px', }); } /** open a dialog to display the GenreCardComponent
+@param name {string}
+@param description {string} */ openGenreDialog(name: string, description: string): void { this.dialog.open(GenreCardComponent, { data: { name: name, description: description }, width: '300px', }); }
+/**
+
+open a dialog to display the DirectorCardComponent
+@param name {string}
+@param bio {string}
+@param birth {string}
+@param death {string} */ openDirectorDialog( name: string, bio: string, birth: string, death: string ): void { this.dialog.open(DirectorCardComponent, { data: { name: name, bio: bio, birth: birth, death: death }, width: '300px', }); }
+/**
+
+open a dialog to display the SynopsisCardComponent
+@param title {string}
+@param description {string} */ openSynopsisDialog(title: string, description: string): void { this.dialog.open(SynopsisCardComponent, { data: { title: title, description: description }, width: '300px', }); } }
